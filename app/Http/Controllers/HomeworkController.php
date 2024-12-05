2fb7d9f59381ff124b2405da7e0ee9d93ef747bc
yt1208 <y.togashi04@gmail.com>
2024-12-05 10:48:35 +0900

    ログインユーザーのwhere句を追記

--- app/Http/Controllers/HomeworkController.php
+++ app/Http/Controllers/HomeworkController.php
@@ -6,15 +6,23 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\Subject;
 use App\Models\Homework;
+use Illuminate\Support\Facades\Auth;
 
 class HomeworkController extends Controller
 {
     public function index()
     {
+    
+    $user = Auth::user();
+    $users = User::where('student_id', $user->student_id)->get();
     $subjects = Subject::orderBy('name')->get();
-    $homeworks = Homework::orderBy('title', 'deadline')->get();
+    $homeworks = Homework::where('user_id', $user->id)
+    ->orderBy('title')
+    ->orderBy('deadline')
+    ->get();
 
     return view('homework_list', [
+        'users' => $users,
         'subjects' => $subjects,
         'homeworks' => $homeworks,
 
