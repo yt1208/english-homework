2ff1046bc1017f485273c0ea0e66649a044ab919
yt1208 <y.togashi04@gmail.com>
2024-12-05 13:04:03 +0900

    use HomeworkControllerの追記

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\HomeworkController;
 
 Route::get('/', function () {
     return view('welcome');
