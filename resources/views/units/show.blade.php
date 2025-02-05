6ed1fb736420cb567cbfa71f6f1b5932b0bb2e5a
yt1208 <y.togashi04@gmail.com>
2025-02-05 09:53:40 +0900

    テストするボタンの実装

--- resources/views/units/show.blade.php
+++ resources/views/units/show.blade.php
@@ -5,5 +5,8 @@
 @section('content')
     <h2>{{ $unit->name }}</h2>
     <p>{!! nl2br(e($description)) !!}</p>
-    <a href="{{ route('units.index') }}">単元一覧に戻る</a>
+    <div class="mt-4">
+    <a href="{{ route('grammar.index', ['slug' => $unit->slug]) }}" class="btn btn-primary">テストする</a>
+    <a href="{{ route('units.index') }}" class="btn btn-primary">単元一覧に戻る</a>
+    </div>
 @endsection
