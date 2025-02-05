0c1c9f1ce64827568a535037652ca6b26b1bed4f
yt1208 <y.togashi04@gmail.com>
2025-02-05 10:26:27 +0900

    単元一覧デザインの修正

--- resources/views/units/index.blade.php
+++ resources/views/units/index.blade.php
@@ -3,14 +3,26 @@
 @section('title', '学習単元一覧')
 
 @section('content')
-    <h1>学習単元一覧</h1>
-    <ul>
-        @foreach ($units as $unit)
-            <li>
-                <a href="{{ url('/units/' . $unit->slug) }}">{{ $unit->name }}</a>
-                <a href="{{ route('grammar.index', ['slug' => $unit->slug]) }}">テストページ</a>
-            </li>
-        @endforeach
-    </ul>
+<div class="container">
+    <h1 class="mb-4 text-start">英検５級の文法</h1>
+
+    <table class="table table-dark table-striped table-hover text-center">
+        <thead>
+            <tr>
+                <th>UNIT</th>
+                <th>STUDY</th>
+                <th>TEST</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($units as $unit)
+            <tr>
+                <td>{{ $unit->name }}</td>
+                <td><a href="{{ url('/units/' . $unit->slug) }}" class="btn btn-info text-white">学習する</a></td>
+                <td><a href="{{ route('grammar.index', ['slug' => $unit->slug]) }}" class="btn text-white" style="background-color: #FFD700;">テストする</a></td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
 @endsection
-                                                                                            
