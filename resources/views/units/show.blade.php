220a211519e3326b248a8a16af9fc22d35f6bff7
yt1208 <y.togashi04@gmail.com>
2025-03-10 15:04:57 +0900

    chatbotの1st fix

--- resources/views/units/show.blade.php
+++ resources/views/units/show.blade.php
@@ -10,6 +10,53 @@
     <div class="mt-4">
         <a href="{{ route('units.index') }}" class="btn btn-back">単元一覧に戻る</a>
         <a href="{{ route('grammar.index', ['slug' => $unit->slug]) }}" class="btn btn-test">テストする</a>
-        <a href="{{ route('chatbot') }}" class="btn btn-primary ml-3">チャットボットを使う</a>
     </div>
+
+    <!-- Chatbot button -->
+    <div class="chatbot-button">
+        <button id="openChatbot">チャットボットに質問する</button>
+    </div>
+
+    <!-- Chatbot window -->
+    <div class="chatbot-window">
+        <div class="chatbot-header">
+            <p>チャットボット サポート</p>
+            <button id="closeChatbot">x</button>
+        </div>
+        <div class="chatbot-messages" id="messages"></div>
+        <div class="chatbot-input">
+            <input type="text" id="userInput" placeholder="質問...">
+            <button id="sendMessage">送信</button>
+        </div>
+    </div>
+
+    <script>
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+
+        $(document).ready(function() {
+            $('#openChatbot').click(function() {
+                $('#openChatbot').fadeOut();
+                $('.chatbot-window').fadeIn();
+            });
+
+            $('#closeChatbot').click(function() {
+                $('.chatbot-window').fadeOut();
+                $('#openChatbot').fadeIn();
+            });
+
+            $('#sendMessage').click(function() {
+                let message = $('#userInput').val();
+                $('#messages').append('<div>あなた: ' + message + '</div>');
+                $.post( "{{ route('ask') }}" , { message: message }, function(data) {
+                    $('#messages').append('<div>Bot: ' + data + '</div>');
+                });
+                $('#userInput').val(''); 
+            });
+        });
+    </script>
+
 @endsection
